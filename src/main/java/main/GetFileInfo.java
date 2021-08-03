5967eb48e64f4e236206d683b7928ef0a276e606
primevibetime <69993408+primevibetime@users.noreply.github.com>
2021-08-03 12:46:26 -0600

    j

--- src/main/java/main/GetFileInfo.java
+++ src/main/java/main/GetFileInfo.java
@@ -66,4 +66,28 @@ public class GetFileInfo {
     public static void setSize(long size) {
         GetFileInfo.size = size;
     }
+
+    public GetFileInfo() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
 }
