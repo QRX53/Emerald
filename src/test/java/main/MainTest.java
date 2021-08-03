a99b54d3e3c8452fef02b5612f6f1f11020dee01
primevibetime <69993408+primevibetime@users.noreply.github.com>
2021-08-03 12:35:07 -0600

    Create MainTest.java

---
+++ src/test/java/main/MainTest.java
@@ -0,0 +1,553 @@
+package main;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void genUUID() {
+    }
+
+    @Test
+    void readFromFile() {
+    }
+
+    @Test
+    void writeToFile() {
+    }
+
+    @Test
+    void getResponse() {
+    }
+
+    @Test
+    void getRandom() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void learnf() {
+    }
+
+    @Test
+    void answer() {
+    }
+
+    @Test
+    void noAnswer() {
+    }
+
+    @Test
+    void tts() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void testIndexOf() {
+    }
+
+    @Test
+    void lastIndexOf() {
+    }
+
+    @Test
+    void testLastIndexOf() {
+    }
+
+    @Test
+    void testIndexOf1() {
+    }
+
+    @Test
+    void testIndexOf2() {
+    }
+
+    @Test
+    void testLastIndexOf1() {
+    }
+
+    @Test
+    void testLastIndexOf2() {
+    }
+
+    @Test
+    void substring() {
+    }
+
+    @Test
+    void testSubstring() {
+    }
+
+    @Test
+    void subSequence() {
+    }
+
+    @Test
+    void concat() {
+    }
+
+    @Test
+    void replace() {
+    }
+
+    @Test
+    void matches() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void replaceFirst() {
+    }
+
+    @Test
+    void replaceAll() {
+    }
+
+    @Test
+    void testReplace() {
+    }
+
+    @Test
+    void split() {
+    }
+
+    @Test
+    void testSplit() {
+    }
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void testJoin() {
+    }
+
+    @Test
+    void toLowerCase() {
+    }
+
+    @Test
+    void testToLowerCase() {
+    }
+
+    @Test
+    void toUpperCase() {
+    }
+
+    @Test
+    void testToUpperCase() {
+    }
+
+    @Test
+    void trim() {
+    }
+
+    @Test
+    void strip() {
+    }
+
+    @Test
+    void stripLeading() {
+    }
+
+    @Test
+    void stripTrailing() {
+    }
+
+    @Test
+    void isBlank() {
+    }
+
+    @Test
+    void lines() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void contentEquals() {
+    }
+
+    @Test
+    void testContentEquals() {
+    }
+
+    @Test
+    void equalsIgnoreCase() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void compareToIgnoreCase() {
+    }
+
+    @Test
+    void regionMatches() {
+    }
+
+    @Test
+    void testRegionMatches() {
+    }
+
+    @Test
+    void startsWith() {
+    }
+
+    @Test
+    void testStartsWith() {
+    }
+
+    @Test
+    void endsWith() {
+    }
+
+    @Test
+    void testClone() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void toPath() {
+    }
+
+    @Test
+    void chars() {
+    }
+
+    @Test
+    void codePoints() {
+    }
+
+    @Test
+    void toCharArray() {
+    }
+
+    @Test
+    void format() {
+    }
+
+    @Test
+    void testFormat() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+
+    @Test
+    void testValueOf() {
+    }
+
+    @Test
+    void testValueOf1() {
+    }
+
+    @Test
+    void copyValueOf() {
+    }
+
+    @Test
+    void testCopyValueOf() {
+    }
+
+    @Test
+    void testValueOf2() {
+    }
+
+    @Test
+    void testValueOf3() {
+    }
+
+    @Test
+    void testValueOf4() {
+    }
+
+    @Test
+    void testValueOf5() {
+    }
+
+    @Test
+    void testValueOf6() {
+    }
+
+    @Test
+    void testValueOf7() {
+    }
+
+    @Test
+    void intern() {
+    }
+
+    @Test
+    void repeat() {
+    }
+
+    @Test
+    void compare() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void createNewFile() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteOnExit() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void testList() {
+    }
+
+    @Test
+    void listFiles() {
+    }
+
+    @Test
+    void testListFiles() {
+    }
+
+    @Test
+    void testListFiles1() {
+    }
+
+    @Test
+    void mkdir() {
+    }
+
+    @Test
+    void mkdirs() {
+    }
+
+    @Test
+    void renameTo() {
+    }
+
+    @Test
+    void setLastModified() {
+    }
+
+    @Test
+    void setReadOnly() {
+    }
+
+    @Test
+    void setWritable() {
+    }
+
+    @Test
+    void testSetWritable() {
+    }
+
+    @Test
+    void setReadable() {
+    }
+
+    @Test
+    void testSetReadable() {
+    }
+
+    @Test
+    void setExecutable() {
+    }
+
+    @Test
+    void testSetExecutable() {
+    }
+
+    @Test
+    void canExecute() {
+    }
+
+    @Test
+    void listRoots() {
+    }
+
+    @Test
+    void getTotalSpace() {
+    }
+
+    @Test
+    void getFreeSpace() {
+    }
+
+    @Test
+    void getUsableSpace() {
+    }
+
+    @Test
+    void createTempFile() {
+    }
+
+    @Test
+    void testCreateTempFile() {
+    }
+
+    @Test
+    void testCompareTo() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void charAt() {
+    }
+
+    @Test
+    void codePointAt() {
+    }
+
+    @Test
+    void codePointBefore() {
+    }
+
+    @Test
+    void codePointCount() {
+    }
+
+    @Test
+    void offsetByCodePoints() {
+    }
+
+    @Test
+    void getChars() {
+    }
+
+    @Test
+    void getBytes() {
+    }
+
+    @Test
+    void testGetBytes() {
+    }
+
+    @Test
+    void testGetBytes1() {
+    }
+
+    @Test
+    void testGetBytes2() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getParent() {
+    }
+
+    @Test
+    void getParentFile() {
+    }
+
+    @Test
+    void getPath() {
+    }
+
+    @Test
+    void isAbsolute() {
+    }
+
+    @Test
+    void getAbsolutePath() {
+    }
+
+    @Test
+    void getAbsoluteFile() {
+    }
+
+    @Test
+    void getCanonicalPath() {
+    }
+
+    @Test
+    void getCanonicalFile() {
+    }
+
+    @Test
+    void toURL() {
+    }
+
+    @Test
+    void toURI() {
+    }
+
+    @Test
+    void canRead() {
+    }
+
+    @Test
+    void canWrite() {
+    }
+
+    @Test
+    void exists() {
+    }
+
+    @Test
+    void isDirectory() {
+    }
+
+    @Test
+    void isFile() {
+    }
+
+    @Test
+    void isHidden() {
+    }
+
+    @Test
+    void lastModified() {
+    }
+
+    @Test
+    void main() {
+    }
+}
